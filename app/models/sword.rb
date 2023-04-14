71ce572e93b3e73f5065a585696cf859db41c513
c.givens <CadeVictor719@gmail.com>
2023-04-14 03:25:27 +0000

    Edits for tests

--- app/models/sword.rb
+++ app/models/sword.rb
@@ -1,8 +1,9 @@
 class Sword < ApplicationRecord
-    validates :type, presence:true
-    validates :subtype, presence:true
-    validates :pommel, presence:true
-    validates :grip, presence:true
-    validates :steel, presence:true
+    validates_presence_of :type, :subtype, :pommel, :grip, :steel
+    #validates :type, presence:true
+    #validates :subtype, presence:true
+    #validates :pommel, presence:true
+    #validates :grip, presence:true
+    #validates :steel, presence:true
     #validates :description, presence:true 
 end
